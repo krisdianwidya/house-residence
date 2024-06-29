adbb99ab1352ce4813632cd3eb5cf2daaab68680
Krisdian Widya Ekatama <krisdianwidya@gmail.com>
2024-06-29 13:08:16 +0700

    finish logic to store new house

--- app/Http/Controllers/Api/HouseController.php
+++ app/Http/Controllers/Api/HouseController.php
@@ -6,12 +6,37 @@ use App\Models\House;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\HouseResource;
+use Illuminate\Support\Facades\Validator;
 
 class HouseController extends Controller
 {
     public function index()
     {
+        // get all houses
         $houses = House::latest()->paginate(5);
+
+        // return collection of houses
         return new HouseResource(true, 'Houses List', $houses);
+    }
+
+    public function store(Request $request)
+    {
+
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'house_number' => 'required|string',
+            'is_active' => 'required|boolean',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //create house
+        $house = House::create($request->all());
+
+        //return response
+        return new HouseResource(true, 'House added!', $house);
     }
 }
