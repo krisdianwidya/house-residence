3ad1c6d0ba7d6129a22f83b94dada42e1afea1eb
Krisdian Widya Ekatama <krisdianwidya@gmail.com>
2024-06-30 08:41:55 +0700

    add api routing for payemnt and person

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\HouseController;
+use App\Http\Controllers\Api\PaymentController;
+use App\Http\Controllers\Api\PersonController;
 
 //posts
 Route::apiResource('/houses', HouseController::class);
+Route::apiResource('/payments', PaymentController::class);
+Route::apiResource('/persons', PersonController::class);
